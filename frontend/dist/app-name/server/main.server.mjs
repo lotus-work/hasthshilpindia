fd42750a9da0585807ecf2627068f7b648fd77bc
lotus-ccm <61588021+lotus-work@users.noreply.github.com>
2025-05-04 12:15:51 +0530

    updating the build files

--- frontend/dist/app-name/server/main.server.mjs
+++ frontend/dist/app-name/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-B6Y5BC4Z.mjs";import"./chunk-ECAF4OTT.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-RGHR7BMW.mjs";import"./chunk-ECAF4OTT.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
