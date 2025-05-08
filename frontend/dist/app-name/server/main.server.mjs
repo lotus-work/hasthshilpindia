872b274636dd5dfacc336dad5ce130e5dce339c8
lotus-ccm <61588021+lotus-work@users.noreply.github.com>
2025-05-08 20:50:11 +0530

    uploading build files

--- frontend/dist/app-name/server/main.server.mjs
+++ frontend/dist/app-name/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-OQWV32YZ.mjs";import"./chunk-ON6EEK6V.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-P2Q3BGX2.mjs";import"./chunk-ON6EEK6V.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
