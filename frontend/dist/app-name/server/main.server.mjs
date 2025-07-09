64ed39f3adf10f107b41024f95f3b9850f1684ac
lotus-ccm <61588021+lotus-work@users.noreply.github.com>
2025-07-09 11:01:36 +0530

    bug fixes categories clickable

--- frontend/dist/app-name/server/main.server.mjs
+++ frontend/dist/app-name/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-RP26KEZB.mjs";import"./chunk-K7GBWFUN.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-FTXGD2UV.mjs";import"./chunk-K7GBWFUN.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
