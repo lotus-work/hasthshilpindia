46e74c28ef9d49c49f6f1191240bf610d7f5933f
lotus-ccm <61588021+l0tusbiswas@users.noreply.github.com>
2025-09-25 02:17:59 +0530

    new build files

--- frontend/dist/app-name/server/main.server.mjs
+++ frontend/dist/app-name/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-AFLYH37P.mjs";import"./chunk-K7GBWFUN.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-RP4HR5NS.mjs";import"./chunk-K7GBWFUN.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
